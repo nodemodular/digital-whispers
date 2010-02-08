b3954c738b1546ae5f313586bba58139aa3348db
naut <neonaut>
2010-02-08 17:04:04 +0100

    fixed conflict

--- ajax/conn.php
+++ ajax/conn.php
@@ -6,7 +6,13 @@
 		$user = "root"; 
 		$pass = "";
 		$db	  = "digitalwhispers";
-
+	
 	}
+	
+	$link = mysql_connect($host, $user, $pass) or die("Keine Verbindung möglich: " . mysql_error());
+	mysql_select_db($db) or die("Auswahl der Datenbank fehlågeschlagen");
+
+	
+
 ?>
 
