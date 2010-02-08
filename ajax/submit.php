0704294575e50a64ba94f7417b7b8390f1569c93
naut <neonaut>
2010-02-08 19:21:38 +0100

    check for correct filetype

--- ajax/submit.php
+++ ajax/submit.php
@@ -8,11 +8,8 @@ $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 	echo $_FILES['userfile'];
 	
-<<<<<<< HEAD
-	$send = "INSERT INTO data (filename) VALUES ('".$_FILES['userfile']['name']."')";
-=======
+
 	$send = "INSERT INTO data (filename, filetype) VALUES ('".$_FILES['userfile']['name']."', '".$_FILES['userfile']['type']."')";
->>>>>>> 38d7a76535b2af5e6b891a3ceef35ac49e601dab
 	mysql_query($send) OR die(mysql_error());
 	
 } else {
