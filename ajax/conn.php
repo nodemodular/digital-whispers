236c6bad0d95f119f9a345598ad568f86d287d25
Tobias Furtschegger <tobiasfurtschegger@tobias-furtscheggers-macbook-pro-15.local>
2010-02-08 18:58:16 +0100

    loader // success notification

--- ajax/conn.php
+++ ajax/conn.php
@@ -3,14 +3,16 @@
 	if ($_SERVER['SERVER_NAME'] == "localhost") {
 						
 		$host = "localhost";
-		$user = "root"; 
-		$pass = "";
-		$db	  = "digitalwhispers";
 	
-	}
+	} 
+
+$user = "root"; 
+$pass = "";
+$db	  = "digitalwhispers";
+
 	
 	$link = mysql_connect($host, $user, $pass) or die("Keine Verbindung möglich: " . mysql_error());
-	mysql_select_db($db) or die("Auswahl der Datenbank fehlågeschlagen");
+	mysql_select_db($db) or die("Auswahl der Datenbank fehlgeschlagen");
 
 	
 
